dbc0034143fe66e08ecee86a4a31989a4cf77e8a
Sontamm <sontam@js.com>
2020-12-23 19:02:22 +0545

    ReduxAdd

--- src/App.js
+++ src/App.js
@@ -5,50 +5,39 @@ import { HomePage } from './pages/homepages/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header-component/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import {connect} from 'react-redux';
 import {auth, createUserProfileDocument} from './firebase/firebase.utils';
+import {setCurrentUser} from './redux/user/user.actions';
 
 class App extends React.Component{
-  constructor(){
-    super();
-    this.state={
-      currentUser : null
-    };
-  } 
-unsubscribeFromAuth=null;
+  unsubscribeFromAuth = null;
 
-componentDidMount=()=>{
-  this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-    if (userAuth) {
-      const userRef = await createUserProfileDocument(userAuth);
+  componentDidMount() {
+    const { setCurrentUser } = this.props;
 
-      userRef.onSnapshot(snapShot => {
-        this.setState({
-          currentUser: {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          setCurrentUser({
             id: snapShot.id,
             ...snapShot.data()
-          }
-        },
-       );
+          });
+        });
+      }
 
-       
-      });
-    }
-else{
-  this.setState({ currentUser: userAuth });
-}
-   
-  });
-}
+      setCurrentUser(userAuth);
+    });
+  }
 
-
-componentWillUnmount(){
-  this.unsubscribeFromAuth();
-}
-
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
   render(){
     return (
       <div >
-        <Header currentUser={this.state.currentUser}/>
+        <Header />
         <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ ShopPage } />
@@ -59,5 +48,7 @@ componentWillUnmount(){
   }
   
 }
-
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser : user =>dispatch(setCurrentUser(user))
+});
+export default connect(null,mapDispatchToProps)(App);
