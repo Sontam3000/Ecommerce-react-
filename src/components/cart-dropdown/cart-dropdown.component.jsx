c8997fd5b4898f9024530bf000c1e94255cca16d
Sontamm <sontam@js.com>
2020-12-24 19:33:40 +0545

    CartFinished

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,21 +3,42 @@ import './cart-dropdown.styles.scss';
 import CustomButton from '../custom-button/custom-button.component';
 import { connect } from 'react-redux';
 import CartItem from '../cart-item/cart-item.component';
+import {createStructuredSelector} from 'reselect';
+import {selectCurrentUser} from '../../redux/user/user.selectors.js';
+import {selectCartItems} from '../../redux/cart/cart.selectors';
+import {withRouter} from 'react-router-dom';
+import { toggleCartHidden} from '../../redux/cart/cart.actions.js';
 
-const CartDropdown=({currentUser ,cartItems})=>(
+const CartDropdown=({currentUser ,cartItems,history,dispatch})=>(
     <div className='cart-dropdown'>
-         <h5>Signned In As:{currentUser.displayName}</h5>
-        <div className='cart-items'>
-            {
-                cartItems.map(cartItem=> <CartItem key={cartItem.id} item={cartItem}  />)
+         
+            { currentUser?
+            <span className='signinstat'>Signned In As:
+                {currentUser.displayName.toUpperCase()}
+                </span>
+                : <span className='signinstat'>Not signed in</span>
             }
+       
+        <div className='cart-items'>
+            {cartItems.length ? (
+                cartItems.map(cartItem=> (<CartItem key={cartItem.id} item={cartItem}  />)
+            )):
+            (<span className='empty-message'> Cart Is Empty </span>)
+
+            }
+            
         </div>
-        <CustomButton >GO TO CHECKOUT</CustomButton>
+        <CustomButton onClick={()=>
+            { 
+                history.push('/checkout');
+                dispatch(toggleCartHidden());
+            }
+           }>GO TO CHECKOUT</CustomButton>
     </div>
 );
-const mapStateToProps = ({user:{currentUser}, cart: {cartItems}}) =>({
-    currentUser,
-    cartItems
+const mapStateToProps = createStructuredSelector({
+    currentUser:selectCurrentUser,
+    cartItems:selectCartItems
 });
   
-export default connect(mapStateToProps)(CartDropdown);
+export default withRouter(connect(mapStateToProps)(CartDropdown));
